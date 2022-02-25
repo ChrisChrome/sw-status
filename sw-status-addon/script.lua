610061c6ebd0924d40e2d74a6331fa1de2406bc7
ChrisChrome <christophercookman@gmail.com>
2022-02-25 00:00:48 -0700

    Update the addon to be a tad bit more consistent

--- sw-status-addon/script.lua
+++ sw-status-addon/script.lua
@@ -4,17 +4,18 @@ ticks_time = 0
 ticks = 0
 
 function onTick(game_ticks)
-	calculateTPS()
-	if ticks % 30 == 0 then
-		server.httpGet(port, "/data?playercount="..#server.getPlayers()-1 .."&tps="..tps)
-	end
-end
-
-function calculateTPS()
-    ticks = ticks + 1
+	ticks = ticks + 1
     if server.getTimeMillisec() - ticks_time >= 500 then
         tps = ticks*2
         ticks = 0
         ticks_time = server.getTimeMillisec()
     end
+	if server.getTimeMillisec() % 500 == 0 then
+		server.announce("HTTPDEBUG", "attempting request")
+		server.httpGet(port, "/data?playercount="..#server.getPlayers()-1 .."&tps="..tps)
+	end
+end
+
+function httpReply(port, url, response_body)
+	server.announce(url..port, response_body)
 end
